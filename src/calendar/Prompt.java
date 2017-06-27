8ec1611dfb83009d3d8553a9c2e8a90ca2b58c30
LHS <LHS@211.35.150.67>
2017-06-28 01:52:32 +0900

    print virtual calendar1

---
+++ src/calendar/Prompt.java
@@ -0,0 +1,41 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+	
+	private final static String PROMPT = "cal> ";
+	
+	public void runPrompt(){
+		
+		Scanner scanner = new Scanner(System.in);
+		calendar cal = new calendar();
+		
+		int month = 1;
+		
+		while(true) {
+			System.out.println("달을 입력하세요.(종료: -1)");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if ( month == -1) {
+				break;
+			}
+			if(month > 12 || month < 1){
+				continue;
+			}
+			cal.printCalendar(2017,month);
+
+		}
+		System.out.println("Bye!!");
+		scanner.close();
+	}
+	
+	public static void main(String[] args) {
+
+		//쉘 실행
+		Prompt p = new Prompt();
+		p.runPrompt();
+
+
+	}
+}

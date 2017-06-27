867a4eb3c27bc7761c2309607116fb6fd5848274
LHS <LHS@211.35.150.67>
2017-06-28 01:37:23 +0900

    myself: print virtual calendar

--- src/calendar/calendar.java
+++ src/calendar/calendar.java
@@ -10,13 +10,19 @@ public class calendar {
 		return MAX_DAYS[month - 1];
 	}
 
-	public void printSampleCalendar() {
+	public void printSampleCalendar(int checknum) {
 		System.out.println(" 일   월  화  수   목  금   토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		if (checknum == 30){
+			System.out.println("29 30");
+		}
+		if (checknum == 31){
+			System.out.println("29 30 31");
+		}
 	}
 
 	public static void main(String[] args) {
@@ -29,7 +35,7 @@ public class calendar {
 		
 		int month = 1;
 		while(true) {
-			System.out.println("달을 입력하세요.");
+			System.out.println("달을 입력하세요.(종료: -1)");
 			System.out.print(PROMPT);
 			month = scanner.nextInt();
 			if ( month == -1) {
@@ -38,7 +44,7 @@ public class calendar {
 			if(month > 12 || month < 1){
 				continue;
 			}
-			System.out.printf("%d월은 %d일 까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+			cal.printSampleCalendar(cal.getMaxDaysOfMonth(month));
 
 		}
 		System.out.println("Bye!!");

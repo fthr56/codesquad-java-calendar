96fb2d7698e1c528f2020ce327941558f1df3ea7
LHS <LHS@DESKTOP-A48G9NV>
2017-06-28 18:30:02 +0900

    update more reality calendar

--- src/calendar/calendar.java
+++ src/calendar/calendar.java
@@ -1,50 +1,52 @@
 package calendar;
 
-import java.util.Scanner;
-
 public class calendar {
 
 	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public boolean isLeapYear(int year){
-		if (year % 4 ==0 && (year % 100 != 0 || year % 400 == 0))
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 			return true;
 		return false;
 	}
-	
-	public int getMaxDaysOfMonth(int year,int month) {
-		if (isLeapYear(year)){
+
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
 			return LEAP_MAX_DAYS[month - 1];
 		}
 		return MAX_DAYS[month - 1];
 	}
 
-	public void printCalendar(int year, int month) {
-		System.out.printf("    <<%4d%3d>>\n", year,month);
+	public void printCalendar(int year, int month, int weekday) {
+		System.out.printf("    <<%4d%3d>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
-		
-		int maxDay = getMaxDaysOfMonth(year,month);
-		
-		for(int i = 1; i <=maxDay; i++){
-			System.out.printf("%3d",i);
-			if(i % 7 ==0)
+
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count < 7) ? count : 0;
+
+		// print first line
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+
+		count++;
+		// print from second line to last
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim)
 				System.out.println();
 		}
 		System.out.println();
-		
-//		System.out.println(" 1  2  3  4  5  6  7");
-//		System.out.println(" 8  9 10 11 12 13 14");
-//		System.out.println("15 16 17 18 19 20 21");
-//		System.out.println("22 23 24 25 26 27 28");
-//		if (month == 30){
-//			System.out.println("29 30");
-//		}
-//		if (month == 31){
-//			System.out.println("29 30 31");
-//		}
+		System.out.println();
+
 	}
 
-	
 }
